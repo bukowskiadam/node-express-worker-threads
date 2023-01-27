2d452d0514a469ce88c8bf1093e82e9e3b364c53
Adam Bukowski <bukowski.adam@gmail.com>
2023-01-27 16:06:43 +0100

    feat: add nested path

--- worker-app.js
+++ worker-app.js
@@ -10,6 +10,11 @@ app.get("/", (request, response) => {
   response.send("finished");
 });
 
+// Just to test nested paths: http://localhost:3033/blocking/test
+app.get("/test", (request, response) => {
+  response.send("test");
+});
+
 const server = app.listen(0, () => {
   const { port } = server.address();
   console.log(`Worker server threadId:${threadId} listening on ${port}`);
